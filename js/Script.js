fb450ce02335a28e982b6b0e3bd2485c931da039
MyUsername <my@email.com>
2022-12-14 14:56:07 +0100

    insert content on shop list

--- js/Script.js
+++ js/Script.js
@@ -3,12 +3,40 @@ import { createApp } from 'vue'
 createApp({
   data() {
     return {
-      
+
       shopList: [
          {
             text: 'burro',
-            done: true
-         }
+            done: true,
+         },
+         {
+            text: 'latte',
+            done: true,
+         },
+         {
+            text: 'riso',
+            done: false,
+         },
+         {
+            text: 'farina',
+            done: true,
+         },
+         {
+            text: 'pellicola',
+            done: false,
+         },
+         {
+            text: 'scottex',
+            done: true,
+         },
+         {
+            text: 'pane',
+            done: true,
+         },
+         {
+            text: 'cioccolata',
+            done: false,
+         },
       ],
 
     }
